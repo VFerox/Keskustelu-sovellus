2445310b216b2544226f4ce25380a7b6a78815cd
Richard R <108690559+VFerox@users.noreply.github.com>
2025-02-02 15:37:32 +0200

    Add files via upload

---
+++ website/static/index.js
@@ -0,0 +1,26 @@
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+}
+
+function likeNote(noteId) {
+    fetch("/like-note", {
+        method: "POST",
+        body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+        window.location.href = "/";
+    });
+}
+
+function dislikeNote(noteId) {
+    fetch("/dislike-note", {
+        method: "POST",
+        body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+        window.location.href = "/";
+    });
+}
